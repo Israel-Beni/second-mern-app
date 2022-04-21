25d7c20547bf28102bb6e0b6021391c224420431
Israel-Beni <bendegbe72@gmail.com>
2022-04-21 18:15:27 +0530

    setting up our database schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,5 +5,22 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
