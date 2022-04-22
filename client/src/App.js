d07138e5faf60f782c4609ed26005df79e725915
Israel-Beni <bendegbe72@gmail.com>
2022-04-22 17:55:17 +0530

    Provider component injected into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,25 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     console.log("process.env.PUBLIC_URL:", process.env.PUBLIC_URL);
     return (
-      <Router basename={'/' + process.env.PUBLIC_URL }>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route  path="/" element={<Landing />} />
-            <Route  path="/register" element={<Register />} />
-            <Route  path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router basename={'/' + process.env.PUBLIC_URL }>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route  path="/" element={<Landing />} />
+              <Route  path="/register" element={<Register />} />
+              <Route  path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 };
