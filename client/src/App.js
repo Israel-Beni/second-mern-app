0327cfa13831275f8faa80faf4c384c150e17909
Israel-Beni <bendegbe72@gmail.com>
2022-04-22 06:49:28 +0530

    Error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router basename={'/' + process.env.PUBLIC_URL }>
+        <div className="App">
+          <Navbar />
+          <Landing />
+        </div>
+      </Router>
     );
   }
 };
