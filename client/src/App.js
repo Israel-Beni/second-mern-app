6aa0f33df21c9db85d873862fdbcabeaa3e34996
Israel-Beni <bendegbe72@gmail.com>
2022-04-22 15:34:41 +0530

    Login and Register components injected into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './componnents/auth/Login';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -12,7 +14,11 @@ class App extends Component {
       <Router basename={'/' + process.env.PUBLIC_URL }>
         <div className="App">
           <Navbar />
-          <Landing />
+          <Routes>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Register} />
+          </Routes>
         </div>
       </Router>
     );
