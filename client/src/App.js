c61cfc8946d440e6abd58f8972944fb5c3bc184d
Israel-Beni <bendegbe72@gmail.com>
2022-04-22 15:59:40 +0530

    typo corrected and static components code completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
-import Login from './componnents/auth/Login';
+import Login from './components/auth/Login';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
+    console.log("process.env.PUBLIC_URL:", process.env.PUBLIC_URL);
     return (
       <Router basename={'/' + process.env.PUBLIC_URL }>
         <div className="App">
           <Navbar />
           <Routes>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Register} />
+            <Route  path="/" element={<Landing />} />
+            <Route  path="/register" element={<Register />} />
+            <Route  path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
